6a5aec011b90324a37a0443bfcc916f9e9a38407
rivasej1372 <rivasej@gmail.com>
2021-09-29 22:29:48 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#Demo").innerHTML = domainGenerator();
 };
+
+function domainGenerator() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; z < noun.length; z++) {
+        console.log(pronoun[x] + adj[y] + noun[z]);
+      }
+    }
+  }
+}
